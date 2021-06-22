60046a35aeff47803514eace256faab6525a9296
dl8sd11 <tmd910607@gmail.com>
2021-06-22 11:35:16 +0800

    delete script for demo

--- src/Rscripts/sample.sh
+++
@@ -1,6 +0,0 @@
-Rscript Rscripts/review_tfidf_graph.R ChIJp4DmtIqpQjQRMc1gEd_aAS4 1 2 
-# 新馬辣經典麻辣鍋-公館店 Low
-
-Rscript Rscripts/review_tfidf_graph.R ChIJp4DmtIqpQjQRMc1gEd_aAS4 4 5
-# 新馬辣經典麻辣鍋-公館店 High
-
